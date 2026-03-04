ebafb5499f1d023502c36b683bb3158c43d7da1f
aditya081105 <aditya08112005@gmail.com>
2026-03-04 20:39:59 +0530

    fix admin static

--- core/settings.py
+++ core/settings.py
@@ -28,6 +28,7 @@ DEBUG = False
 ALLOWED_HOSTS = ['192.168.29.48', '127.0.0.1', "ziradei-matrimony.onrender.com"]
 CSRF_TRUSTED_ORIGINS = [
     "https://ziradei-matrimony.onrender.com",
+    "https://*.onrender.com",
 ]
 
 
