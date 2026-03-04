05ec590a98659f5cb3f7769c475d099b9a6abf6a
aditya081105 <aditya08112005@gmail.com>
2026-03-04 20:45:38 +0530

    rm smth

--- core/settings.py
+++ core/settings.py
@@ -28,7 +28,6 @@ DEBUG = False
 ALLOWED_HOSTS = ['192.168.29.48', '127.0.0.1', "ziradei-matrimony.onrender.com"]
 CSRF_TRUSTED_ORIGINS = [
     "https://ziradei-matrimony.onrender.com",
-    "https://*.onrender.com",
 ]
 
 
