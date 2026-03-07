72423cd39082ad664494be5e02393a7b5d920281
aditya081105 <aditya08112005@gmail.com>
2026-03-07 11:15:20 +0530

    db2

--- core/settings.py
+++ core/settings.py
@@ -73,20 +73,8 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-if os.getenv("DATABASE_URL"):
-    DATABASES = {
-        "default": dj_database_url.config(
-            default=os.getenv("DATABASE_URL"),
-            conn_max_age=600
-        )
-    }
-else:
-    DATABASES = {
-        "default": {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": BASE_DIR / "db.sqlite3",
-        }
-    }
+DATABASE_URL= os.getenv("DATABASE_URL")
+    
 
 
 # Password validation
