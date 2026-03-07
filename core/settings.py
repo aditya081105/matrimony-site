f3e2412bfd7060126191aa400bb1de60b707f9f6
aditya081105 <aditya08112005@gmail.com>
2026-03-07 10:24:09 +0530

    remove manual cloudinary config

--- core/settings.py
+++ core/settings.py
@@ -22,16 +22,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-import cloudinary
-
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET"),
-    secure=True
-)
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
