bb5ebc2fb717a94776645eac26e8644b397488e1
aditya081105 <aditya08112005@gmail.com>
2026-03-21 13:56:49 +0530

    comt

--- core/settings.py
+++ core/settings.py
@@ -151,9 +151,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "django.contrib.sites",
-    "allauth",
-    "allauth.account",
     # Your Apps
     'users',
     'search',
