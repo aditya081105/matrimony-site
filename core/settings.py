dc43cf8430b90a6c1a0e5a466d98710daa145a1a
aditya081105 <aditya08112005@gmail.com>
2026-03-23 06:49:59 +0530

    fix duplicate migration

--- core/settings.py
+++ core/settings.py
@@ -67,12 +67,20 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-DATABASES = {
-    "default": dj_database_url.config(
-        default="sqlite:///" + str(BASE_DIR / "db.sqlite3"),
-        conn_max_age=600
-    )
-}
+if os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": dj_database_url.config(
+            default=os.getenv("DATABASE_URL"),
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 DATABASE_URL = os.getenv("DATABASE_URL")
     
 
