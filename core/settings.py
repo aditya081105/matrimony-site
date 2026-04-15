6b56b99d424e47ef856c6cb50e617debc8e8528f
aditya081105 <aditya08112005@gmail.com>
2026-04-15 20:16:28 +0530

    fixtry1

--- core/settings.py
+++ core/settings.py
@@ -52,7 +52,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
-                #'users.context_processors.pending_request_count',
+                'users.context_processors.pending_request_count',
             ],
         },
     },
