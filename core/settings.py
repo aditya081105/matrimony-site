72277395f313fe0d17bcdc84694f96a59f7f72c9
aditya081105 <aditya08112005@gmail.com>
2026-04-15 19:44:52 +0530

    no db

--- core/settings.py
+++ core/settings.py
@@ -67,12 +67,12 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 
-DATABASES = {
-    'default': dj_database_url.config(
-        default=os.getenv('DATABASE_URL'),
-        conn_max_age=600,
-    )
-}
+#DATABASES = {
+#    'default': dj_database_url.config(
+#        default=os.getenv('DATABASE_URL'),
+#        conn_max_age=600,
+#   )
+#}
     
 
 
