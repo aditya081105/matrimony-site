8563e674157e99deecbe206e2951fbd71bba410a
aditya081105 <aditya08112005@gmail.com>
2026-04-16 09:58:21 +0530

    debugg false

--- core/settings.py
+++ core/settings.py
@@ -19,7 +19,7 @@ load_dotenv()
 SECRET_KEY = os.getenv("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['192.168.29.48', '127.0.0.1', "siwan-matrimony.onrender.com"]
 CSRF_TRUSTED_ORIGINS = [
