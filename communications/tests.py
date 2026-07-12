f03a4ecc00c979a3b5a5f6868a73c4fb70653576
aditya081105 <aditya08112005@gmail.com>
2026-07-12 17:45:54 +0530

    Add automated tests

--- communications/tests.py
+++ communications/tests.py
@@ -1,47 +1,92 @@
 from django.test import TestCase
-from users.models import CustomUser
-from .models import Block, ContactRequest, SavedProfile
+from django.urls import reverse
 
+from users.models import CustomUser, City
+from communications.models import (
+    Block,
+    ContactRequest,
+    RequestAttempt,
+    SavedProfile,
+)
 
-class CommunicationModelTests(TestCase):
-
+class CommunicationTests(TestCase):
     def setUp(self):
+        self.city = City.objects.create(name="Delhi")
+
         self.user1 = CustomUser.objects.create_user(
             username="user1",
             email="user1@test.com",
-            password="Test12345!"
+            password="Test12345!",
+            full_name="User One",
+            gender="M",
+            phone_number="9999999999",
+            is_approved=True,
+            is_email_verified=True,
+            date_of_birth="2000-01-01",
+            height_cm=170,
+            city=self.city,
+            profile_photo="test.jpg"
         )
 
         self.user2 = CustomUser.objects.create_user(
             username="user2",
             email="user2@test.com",
-            password="Test12345!"
+            password="Test12345!",
+            full_name="User Two",
+            gender="F",
+            phone_number="8888888888",
+            is_approved=True,
+            is_email_verified=True,
+            date_of_birth="2000-01-01",
+            height_cm=165,
+            city=self.city,
+            profile_photo="test.jpg"
         )
 
-    def test_contact_request_creation(self):
-        request = ContactRequest.objects.create(
+
+    def test_duplicate_request_not_created(self):
+        self.client.login(username="user1", password="Test12345!")
+
+        ContactRequest.objects.create(
             sender=self.user1,
             receiver=self.user2
         )
 
-        self.assertEqual(request.sender, self.user1)
-        self.assertEqual(request.receiver, self.user2)
-        self.assertEqual(request.status, "pending")
+        self.client.get(reverse("send_request", args=[self.user2.id]))
 
-    def test_block_creation(self):
-        block = Block.objects.create(
-            blocker=self.user1,
-            blocked=self.user2
+        self.assertEqual(ContactRequest.objects.count(), 1)
+
+
+    def test_cannot_send_request_to_self(self):
+        self.client.login(username="user1", password="Test12345!")
+
+        self.client.get(reverse("send_request", args=[self.user1.id]))
+
+        self.assertEqual(ContactRequest.objects.count(), 0)
+
+
+    def test_blocked_user_cannot_send_request(self):
+        self.client.login(username="user1", password="Test12345!")
+
+        Block.objects.create(
+            blocker=self.user2,
+            blocked=self.user1
         )
 
-        self.assertEqual(block.blocker, self.user1)
-        self.assertEqual(block.blocked, self.user2)
+        self.client.get(reverse("send_request", args=[self.user2.id]))
 
-    def test_saved_profile_creation(self):
-        saved = SavedProfile.objects.create(
-            user=self.user1,
-            saved_user=self.user2
-        )
+        self.assertEqual(ContactRequest.objects.count(), 0)
 
-        self.assertEqual(saved.user, self.user1)
-        self.assertEqual(saved.saved_user, self.user2)
+
+    def test_daily_request_limit(self):
+        self.client.login(username="user1", password="Test12345!")
+
+        for _ in range(3):
+            RequestAttempt.objects.create(
+                sender=self.user1,
+                receiver=self.user2
+            )
+
+        self.client.get(reverse("send_request", args=[self.user2.id]))
+
+        self.assertEqual(ContactRequest.objects.count(), 0)
